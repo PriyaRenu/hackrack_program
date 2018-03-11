e32e6ca4cdc2fd549c9da795449c816a4fc7ca3c
priya_RS <32307786+PriyaRenu@users.noreply.github.com>
2018-03-11 21:57:23 +0530

    calculate a bill amount

--- meal_cost.cs
+++ meal_cost.cs
@@ -1,3 +1,4 @@
+/*C# Program - To calculate a meal cost of the food in the hotel*/
 using System;
 namespace Rextester
 {
@@ -5,16 +6,16 @@ namespace Rextester
     {
         public static void Main(string[] args)
         {
-			double totalcost;
-	   double meal_cost = Convert.ToDouble(Console.ReadLine());
+	double totalcost;
+	double meal_cost = Convert.ToDouble(Console.ReadLine());
         int tip_percent = Convert.ToInt32(Console.ReadLine());
         int tax_percent = Convert.ToInt32(Console.ReadLine());
-		double a=	Convert.ToDouble(tip_percent);
-			double b=	Convert.ToDouble(tax_percent);
+	double a=Convert.ToDouble(tip_percent);
+	double b=Convert.ToDouble(tax_percent);
         double c=meal_cost*a/100;
         double d=meal_cost*b/100;
         totalcost =meal_cost+c+d;
-			 Console.WriteLine(totalcost);
+	Console.WriteLine(totalcost);
         totalcost = Math.Round(totalcost, 0);
         Console.WriteLine(totalcost);
          }
